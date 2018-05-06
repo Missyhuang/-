c4006fee5a572635a6d457e77a2e7b62896e88d4
Missy <missy@missyde-macbook-pro-9285.local>
2018-05-06 22:39:41 +0800

    加減成數 小數點進位 谷歌小姐

--- numberCalculated/ViewController.swift
+++ numberCalculated/ViewController.swift
@@ -7,9 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
+class ViewController: UIViewController,UITextFieldDelegate {
 
-class ViewController: UIViewController {
-
+    func textFieldShouldReturn(_ textField:UITextField)->Bool{
+        textField.resignFirstResponder()
+        return true
+    }
+    @IBOutlet weak var firstNumTextField: UITextField!
+    @IBOutlet weak var secondNumTextField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBAction func calculatedSeg(_ sender: UISegmentedControl) {
+        let number1:Double
+        var number2:Double
+        var total:Double
+        number1 = Double(firstNumTextField.text!)!
+        number2 = Double(secondNumTextField.text!)!
+        if sender.selectedSegmentIndex == 0 {
+            total = number1 + number2
+        }
+        else if sender.selectedSegmentIndex == 1 {
+            //number1 = Double(firstNumTextField.text!)!
+            //number2 = Double(secondNumTextField.text!)!
+            total = number1 - number2
+            //totalLabel.text = total.description
+            
+        }
+        else if sender.selectedSegmentIndex == 2 {
+            total = number1 * number2
+        }
+        else {
+            total = number1 / number2
+            //total = round(total * 100)/100//小數點第三位四捨五入
+            total = floor(total * 100)/100//小數點第三位無條件捨去
+            //total = ceil(total * 100)/100//小數點第三位無條件進位
+        }
+        totalLabel.text = total.description
+        let speech = AVSpeechUtterance(string: totalLabel.text!)
+        speech.voice = AVSpeechSynthesisVoice(language: "zh-TW")
+        let  synthesizer  =  AVSpeechSynthesizer()
+        synthesizer.speak(speech)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
